f9b972c95d096845b225cfa2a7a5410afab87c4e
crypticsymmetry <ccbb121@gmail.com>
2024-07-05 18:34:34 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 const upload = multer({ dest: 'images/' });
 
 setupDatabase();
