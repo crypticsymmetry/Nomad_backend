5cb8e24e5bfeac1b10f265105747db4fd5f201bf
crypticsymmetry <ccbb121@gmail.com>
2024-07-05 19:53:26 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const upload = multer({ dest: 'images/' });
 
 setupDatabase();
 
-app.use(cors());
+app.use(cors({ origin: 'https://nomad-frontend-miv9tvpmx-crypticsymmetrys-projects.vercel.app/' }));
 app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
