3ceb0344ad0a0a1f3f26e3b5d02e1143300f7063
crypticsymmetry <ccbb121@gmail.com>
2024-07-06 18:36:09 -0700

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,18 +1,11 @@
-const firebase = require("firebase/app");
-require("firebase/firestore");
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json'); // Ensure you have this file from Firebase console
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCEjiw2NdpMSIuv0_VKXHzlrMywAyMtYg4",
-    authDomain: "nomadpowersports-84e70.firebaseapp.com",
-    projectId: "nomadpowersports-84e70",
-    storageBucket: "nomadpowersports-84e70.appspot.com",
-    messagingSenderId: "476619609513",
-    appId: "1:476619609513:web:15a6bcc9a8677a9e0ce0b8",
-    measurementId: "G-M1D1T3QZRX"
-  };
-  
-firebase.initializeApp(firebaseConfig);
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "nomadpowersports-84e70" // replace with your project ID
+});
 
-const db = firebase.firestore();
+const db = admin.firestore();
 
 module.exports = db;
