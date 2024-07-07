7b3413cda3ec2e5544c5a493bba1dfe78c557881
crypticsymmetry <ccbb121@gmail.com>
2024-07-07 02:24:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -66,45 +66,17 @@ app.delete('/machines/:id', (req, res) => {
 
 
 // Upload machine photo
-app.post('/machines/:id/photo', upload.single('photo'), async (req, res) => {
-    try {
-        const { id } = req.params;
-        const file = req.file;
-
-        if (!file) {
-            console.error('No file uploaded.');
-            return res.status(400).send('No file uploaded.');
-        }
-
-        const blob = bucket.file(`images/${file.originalname}`);
-        const blobStream = blob.createWriteStream({
-            metadata: {
-                contentType: file.mimetype
-            }
-        });
-
-        blobStream.on('error', (err) => {
-            console.error('Blob stream error:', err);
-            res.status(500).send('Unable to upload image.');
-        });
-
-        blobStream.on('finish', async () => {
-            const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
-
-            try {
-                await db.collection('machines').doc(id).update({ photo: publicUrl });
-                res.status(200).send({ photo: publicUrl });
-            } catch (err) {
-                console.error('Error updating Firestore:', err);
-                res.status(500).send('Error updating Firestore.');
-            }
-        });
-
-        blobStream.end(file.buffer);
-    } catch (err) {
-        console.error('Error uploading photo:', err);
-        res.status(500).send('Server error.');
-    }
+app.post('/machines/:id/photo', upload.single('photo'), (req, res) => {
+    const { id } = req.params;
+    const photoPath = `/images/${req.file.filename}`;
+    db.collection('machines').doc(id).update({ photo: photoPath })
+    .then(() => {
+        res.status(200).send({ photo: photoPath });
+    })
+    .catch(err => {
+        console.error('Error uploading photo:', err.message);
+        res.status(500).send(err.message);
+    });
 });
 
 // Timer functions
