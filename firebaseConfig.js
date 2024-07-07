ff5c7da49621c5818d51a56e82bb5497ca7b29f7
crypticsymmetry <ccbb121@gmail.com>
2024-07-07 16:16:11 -0700

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,21 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); // Ensure the correct path
+const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: "nomadpowersports-84e70" // Replace with your project ID
-});
+console.log('Initializing Firebase Admin SDK...');
+
+try {
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        storageBucket: "nomadpowersports-84e70.appspot.com"
+    });
+
+    console.log('Firebase Admin SDK initialized successfully.');
+} catch (error) {
+    console.error('Error initializing Firebase Admin SDK:', error);
+}
 
 const db = admin.firestore();
 const bucket = admin.storage().bucket();
 
 module.exports = { db, bucket };
+
